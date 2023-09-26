0e7da18e41e7d66b0ce74257868140946349096f
Joel Grant <joelmgrant@me.com>
2023-09-25 20:30:15 -0600

    resolve tests

--- functions.js
+++ functions.js
@@ -1,9 +1,29 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function sayHello(name){
+    if(name != 'Will') {
+        return "Hi there " + name + "!";
+    } else {
+        return "No more testing Will!";
+    }
+}
+
+function buildCar(color, type) {
+    if (color === undefined && type === undefined) {
+        return {}
+    }
+    
+    return {
+        color: color,
+        type: type
+    }
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
